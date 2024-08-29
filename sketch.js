921ca201f1d5a806fc48b0ccdc89bed49ced1188
bzkbololo <bololobzk@gmail.com>
2024-08-29 11:50:45 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,66 +1,64 @@
-/*
- * @name Objects 2
- * @arialabel  4 sets of vertical white lines that form rectangles of different sizes on a black background. They move around as the user’s mouse moves 
- * @description Ported from example by hbarragan. Move the cursor across the
- * image to change the speed and positions of the geometry. The class MRect
- * defines a group of lines.
- */
 
-let r1, r2, r3, r4;
+   /*
+ * @name Clock
+ * @arialabel Functioning pink clock on a grey background
+ * @description The current time can be read with the second(),
+ * minute(), and hour() functions. In this example, sin() and
+ * cos() values are used to set the position of the hands.
+ */
+let cx, cy;
+let secondsRadius;
+let minutesRadius;
+let hoursRadius;
+let clockDiameter;
 
 function setup() {
-createCanvas(710, 400);
-fill(255, 204);
-noStroke();
-r1 = new MRect(1, 134.0, 0.532, 0.1 * height, 10.0, 60.0);
-r2 = new MRect(2, 44.0, 0.166, 0.3 * height, 5.0, 50.0);
-r3 = new MRect(2, 58.0, 0.332, 0.4 * height, 10.0, 35.0);
-r4 = new MRect(1, 120.0, 0.0498, 0.9 * height, 15.0, 60.0);
+  createCanvas(720, 400);
+  stroke(255);
+
+  let radius = min(width, height) / 2;
+  secondsRadius = radius * 0.71;
+  minutesRadius = radius * 0.6;
+  hoursRadius = radius * 0.5;
+  clockDiameter = radius * 1.7;
+
+  cx = width / 2;
+  cy = height / 2;
 }
 
 function draw() {
-background(0);
+  background(230);
 
-r1.display();
-r2.display();
-r3.display();
-r4.display();
+  // Draw the clock background
+  noStroke();
+  fill(244, 122, 158);
+  ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
+  fill(237, 34, 93);
+  ellipse(cx, cy, clockDiameter, clockDiameter);
 
-r1.move(mouseX - width / 2, mouseY + height * 0.1, 30);
-r2.move((mouseX + width * 0.05) % width, mouseY + height * 0.025, 20);
-r3.move(mouseX / 4, mouseY - height * 0.025, 40);
-r4.move(mouseX - width / 2, height - mouseY, 50);
-}
+  // Angles for sin() and cos() start at 3 o'clock;
+  // subtract HALF_PI to make them start at the top
+  let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+  let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
 
-class MRect {
-    constructor(iw, ixp, ih, iyp, id, it) {
-    this.w = iw; // single bar width
-    this.xpos = ixp; // rect xposition
-    this.h = ih; // rect height
-    this.ypos = iyp; // rect yposition
-    this.d = id; // single bar distance
-    this.t = it; // number of bars
-    }
+  // Draw the hands of the clock
+  stroke(255);
+  strokeWeight(1);
+  line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
+  strokeWeight(2);
+  line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
+  strokeWeight(4);
+  line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
 
-    move(posX, posY, damping) {
-        let dif = this.ypos - posY;
-        if (abs(dif) > 1) {
-            this.ypos -= dif / damping;
-        }
-        dif = this.xpos - posX;
-        if (abs(dif) > 1) {
-            this.xpos -= dif / damping;
-        }
-    }
-
-    display() {
-        for (let i = 0; i < this.t; i++) {
-            rect(
-            this.xpos + i * (this.d + this.w),
-            this.ypos,
-            this.w,
-            height * this.h
-            );
-        }
-    }
+  // Draw the minute ticks
+  strokeWeight(2);
+  beginShape(POINTS);
+  for (let a = 0; a < 360; a += 6) {
+    let angle = radians(a);
+    let x = cx + cos(angle) * secondsRadius;
+    let y = cy + sin(angle) * secondsRadius;
+    vertex(x, y);
+  }
+  endShape();
 }
